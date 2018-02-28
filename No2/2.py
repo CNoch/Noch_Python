0294b49ea4d206bd1066b4453c9869ec2cd9a2e6
xiuyuan chen <wr389110442@outlook.com>
2018-02-28 17:08:50 +0800

    优惠卷问题

---
+++ No2/2.py
@@ -0,0 +1,25 @@
+import base64
+
+#base64编码方便使用
+
+#通过id检验优惠券是否存在，通过goods查找商品
+coupon = {'id':'1231','goods':'0001'}
+
+def gen_coupon(id,goods):
+    pass
+
+def save_coupon(c_code):
+    pass
+
+def show_coupon(c_code):
+    pass
+
+def parse_coupon(c_code):
+    pass
+
+def gen_all():
+    for i in range(1000,1200):
+        c_code = 
+
+if __name__ == '__main__':
+    pass
